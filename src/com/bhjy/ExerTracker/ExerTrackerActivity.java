5b2bff59f16abe726e6c5a53c5139d7a5193381d
Brian Hamilton <bhamilto@udel.edu>
2012-04-25 23:49:48 -0400

    I started to create the database. So far all it has is the table of exercise types. I created a way to save new exercises to the database, but it isn't quite working. Not sure why yet.

--- src/com/bhjy/ExerTracker/ExerTrackerActivity.java
+++ src/com/bhjy/ExerTracker/ExerTrackerActivity.java
@@ -1,5 +1,10 @@
 package com.bhjy.ExerTracker;
 
+import java.util.List;
+
+import com.bhjy.ExerTracker.Database.ExercisesDataSource;
+import com.bhjy.ExerTracker.Models.Exercise;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TabHost;
@@ -15,6 +21,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ExerTrackerActivity extends Activity {
+    
+    private ExercisesDataSource exercisesDB;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,8 +49,14 @@ public class ExerTrackerActivity extends Activity {
         //Create the list of exercises
         final String[] EXERCISES = new String[] {"Pushups", "Pullups", "Situps", "Squats"};
         
+        exercisesDB = new ExercisesDataSource(this);
+        exercisesDB.open();
+
+		List<Exercise> allExercises = exercisesDB.getAllExercises();
+		
         ListView exerciseList = (ListView) this.findViewById(R.id.listView1);
-        exerciseList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, EXERCISES));
+        exerciseList.setAdapter(new ArrayAdapter<Exercise>(this, android.R.layout.simple_list_item_1, allExercises));
+        //exerciseList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, EXERCISES));
         exerciseList.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
               // When clicked, show a toast with the TextView text
@@ -53,5 +68,28 @@ public class ExerTrackerActivity extends Activity {
             }
           });
         
+        Button addExerciseButton = (Button) this.findViewById(R.id.newExerciseButton);
+        addExerciseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String actionName = "com.bhjy.ExerTracker.ShowAddExerciseActivity";
+	        	Intent intent = new Intent(actionName);
+	        	startActivity(intent);				
+			}
+		});
+        
+        
     }
+    
+    @Override
+	protected void onResume() {
+		exercisesDB.open();
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		exercisesDB.close();
+		super.onPause();
+	}
 }
