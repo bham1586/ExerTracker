e2f596842ed981324ed2b8d7ab6da5957dacdd11
Brian Hamilton <bhamilto@udel.edu>
2012-04-25 23:54:07 -0400

    Started to create the database

---
+++ src/com/bhjy/ExerTracker/Models/Exercise.java
@@ -0,0 +1,37 @@
+package com.bhjy.ExerTracker.Models;
+
+public class Exercise {
+	private long id;
+	private String name;
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// Will be used by the ArrayAdapter in the ListView
+	@Override
+	public String toString() {
+		return name;
+	}
+}
